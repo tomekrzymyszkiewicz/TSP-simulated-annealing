66def272ba1ffee00986e9ffc14158b1026a248f
tomekrzymyszkiewicz <tomek.rzymyszkiewicz@gmail.com>
2021-12-16 20:02:04 +0100

    mem usage func deleted

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@
 #include <string>
 #include <sstream>
 #include <algorithm>
-#include <unistd.h>
 #include <iomanip>
 #include "structures.h"
 using namespace std;
@@ -21,34 +20,6 @@ int number_of_current_graph_vertices = 0;
 adjacency_matrix current_graph_adjacency_matrix = adjacency_matrix();
 int *path_array;
 int ***solved_subproblems;
-
-void process_mem_usage(double &vm_usage, double &resident_set)
-{
-    using std::ifstream;
-    using std::ios_base;
-    using std::string;
-
-    vm_usage = 0.0;
-    resident_set = 0.0;
-
-    ifstream stat_stream("/proc/self/stat", ios_base::in);
-
-    string pid, comm, state, ppid, pgrp, session, tty_nr;
-    string tpgid, flags, minflt, cminflt, majflt, cmajflt;
-    string utime, stime, cutime, cstime, priority, nice;
-    string O, itrealvalue, starttime;
-
-    unsigned long vsize;
-    long rss;
-
-    stat_stream >> pid >> comm >> state >> ppid >> pgrp >> session >> tty_nr >> tpgid >> flags >> minflt >> cminflt >> majflt >> cmajflt >> utime >> stime >> cutime >> cstime >> priority >> nice >> O >> itrealvalue >> starttime >> vsize >> rss;
-
-    stat_stream.close();
-
-    long page_size_kb = sysconf(_SC_PAGE_SIZE) / 1024;
-    vm_usage = vsize / 1024.0;
-    resident_set = rss * page_size_kb;
-}
 
 struct Result
 {
